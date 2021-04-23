e75e474ee16d8621d0d593be0ba998cedcd54f44
baekCode <baekcode@gmail.com>
2021-04-24 00:21:20 +0900

    feat : GlobalStyles

---
+++ src/lib/GlobalStyles.js
@@ -0,0 +1,34 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  
+  html {
+    height: 100%;
+  }
+  
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    min-height: 100%;
+  }
+  #root {
+    min-height: 100%;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',monospace;
+  }
+`;
+
+export default GlobalStyle;
