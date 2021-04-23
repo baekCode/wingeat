986fba759308d421779994145065d5dadbe37e83
baekCode <baekcode@gmail.com>
2021-04-24 08:04:06 +0900

    feat : Header UI 구현

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Cart, Container, Inner, Logo, TopBar} from '@components/header/styled';
+import {Cart, Container, Contents, Count, Inner, Logo, TopBar} from '@components/header/styled';
 
 Header.propTypes = {};
 
-function Header(props) {
+function Header({count}) {
   return (
     <Container>
-      <TopBar><Inner><Cart to={'/cart'}>장바구니</Cart></Inner></TopBar>
-      <Logo to={'/'}>로고</Logo>
+      <TopBar><Inner><Cart to={'/cart'}><Count>0</Count>장바구니</Cart></Inner></TopBar>
+      <Contents><Logo to={'/'}><img src={'https://image.wingeat.com/logo/images/we_logo_center.png'}/></Logo></Contents>
     </Container>
   );
 }
--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -2,7 +2,16 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import Responsive from '@components/responsive';
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  border-bottom: 1px solid #d9d9d9;
+`;
+
+export const Contents = styled(Responsive)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem 0;
+`;
 
 export const Inner = styled(Responsive)`
   display: flex;
@@ -15,8 +24,29 @@ export const TopBar = styled.div`
   background: #f5f5f5;
 `;
 
-export const Logo = styled(Link)``;
+export const Logo = styled(Link)`
+  img {
+    max-width: 135px;
+    height: auto;
+  }
+`;
 
 export const Cart = styled(Link)`
-  font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 0.7rem;
+  color: #666;
+`;
+
+export const Count = styled.span`
+  min-width: 0.9rem;
+  min-height: 0.9rem;
+  margin-right:0.1rem;
+  font-size: 0.6rem;
+  color: #fff;
+  line-height: 0.9rem;
+  text-align: center;
+  border-radius: 10px;
+  background:#ff3f42
 `;
