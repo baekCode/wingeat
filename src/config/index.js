06d0808df542460f4a625af91baca3dc76d6e374
baekCode <baekcode@gmail.com>
2021-04-24 23:04:57 +0900

    feat : config env

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/**
+ * env ignore 처리로 인해 주석으로 남김
+ * REACT_APP_API_URL=https://node.wingeat.com/test-api/
+ * REACT_APP_IMG_URL=https://image.wingeat.com/
+ */
+export default {
+  API_URL: process.env.REACT_APP_API_URL,
+  IMG_URL: process.env.REACT_APP_IMG_URL
+};
