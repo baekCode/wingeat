1c639d6345a0003a438eedb59370596285c054d6
baekCode <baekcode@gmail.com>
2021-04-24 10:54:45 +0900

    feat : ListPage ListContainer

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Layout from '@components/layout';
 import CarouselContainer from '@containers/list/CarouselContainer';
+import ListContainer from '@containers/list/ListContainer';
 
 ListPage.propTypes = {};
 
 function ListPage(props) {
   return (
-    <Layout>ListPage
+    <Layout>
       <CarouselContainer/>
+      <ListContainer/>
     </Layout>
   );
 }
