a2387bbd33be51c3596ec2c65bf712fd3e1f55b0
baekCode <baekcode@gmail.com>
2021-04-24 23:06:36 +0900

    feat : list module

---
+++ src/modules/list.js
@@ -0,0 +1,37 @@
+import {createAction, handleActions} from 'redux-actions';
+import {takeLatest} from 'redux-saga/effects';
+import createRequestSaga, {createRequestActionTypes} from '@lib/createRequestSaga';
+import * as listAPI from '@lib/api';
+
+const [LIST, LIST_SUCCESS, LIST_FAILURE] = createRequestActionTypes('list/LIST');
+
+export const getList = createAction(LIST);
+
+const getListSaga = createRequestSaga(LIST, listAPI.getList);
+
+export function* listSaga() {
+  yield takeLatest(LIST, getListSaga);
+}
+
+const initialState = {
+  list : null,
+  error: null
+};
+
+const list = handleActions({
+  [LIST]        : state => ({
+    ...state,
+    list : null,
+    error: null,
+  }),
+  [LIST_SUCCESS]: (state, {payload: list}) => ({
+    ...state,
+    list
+  }),
+  [LIST_FAILURE]: (state, {payload: error}) => ({
+    ...state,
+    error
+  })
+}, initialState);
+
+export default list;
