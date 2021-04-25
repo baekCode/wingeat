59395cb5f2c7ce9e13853de90ddd366d1aaa5bdf
baekCode <baekcode@gmail.com>
2021-04-25 22:55:54 +0900

    feat : Cart Item, price comma

--- src/components/cartList/CartItem.js
+++ src/components/cartList/CartItem.js
@@ -15,17 +15,21 @@ import {
 CartItem.propTypes = {};
 
 function CartItem({item}) {
+  const {image, itemName, price, count} = item;
   const {IMG_URL} = config;
+  const totalPrice = price * count;
+  const priceComma = price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+  const totalPriceComma = totalPrice.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
   return (
     <ItemContainer>
       <ItemCheckbox type={'checkbox'}/>
       <ItemContents>
-        <ItemThumbnail src={`${IMG_URL}${item.image}`}/>
+        <ItemThumbnail src={`${IMG_URL}${image}`}/>
         <ItemInfo>
-          <ItemTitle children={item.itemName}/>
-          <ItemPrice children={item.price}/>
-          <ItemQuantity>{item.count}</ItemQuantity>
-          <ItemTotalPrice>{item.price * item.count}</ItemTotalPrice>
+          <ItemTitle children={itemName}/>
+          <ItemPrice children={priceComma}/>
+          <ItemQuantity>{count}</ItemQuantity>
+          <ItemTotalPrice>{totalPriceComma}</ItemTotalPrice>
         </ItemInfo>
       </ItemContents>
 
