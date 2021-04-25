d8f724bfbb74ec5ded7b3a52131110f7050966e0
baekCode <baekcode@gmail.com>
2021-04-25 23:55:31 +0900

    feat : 장바구니 리스트 & 장바구니 아이템

--- src/components/cartList/CartItem.js
+++ src/components/cartList/CartItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import config from '@config';
 import {
   ItemCheckbox,
@@ -17,9 +17,13 @@ CartItem.propTypes = {};
 function CartItem({item}) {
   const {image, itemName, price, count} = item;
   const {IMG_URL} = config;
-  const totalPrice = price * count;
+  const [quantity, setQuantity] = useState(count);
+  const totalPrice = price * quantity;
   const priceComma = price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
   const totalPriceComma = totalPrice.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+
+  const increase = () => setQuantity(quantity + 1);
+  const decrease = () => setQuantity(quantity === 1 ? 1 : quantity - 1);
   return (
     <ItemContainer>
       <ItemCheckbox type={'checkbox'}/>
@@ -27,12 +31,15 @@ function CartItem({item}) {
         <ItemThumbnail src={`${IMG_URL}${image}`}/>
         <ItemInfo>
           <ItemTitle children={itemName}/>
-          <ItemPrice children={priceComma}/>
-          <ItemQuantity>{count}</ItemQuantity>
-          <ItemTotalPrice>{totalPriceComma}</ItemTotalPrice>
+          <ItemPrice><span>가격 : </span>{priceComma}</ItemPrice>
+          <ItemQuantity><span>수량 : </span>
+            <button onClick={decrease}>-</button>
+            {quantity}
+            <button onClick={increase}>+</button>
+          </ItemQuantity>
+          <ItemTotalPrice><span>합계 : </span>{totalPriceComma}</ItemTotalPrice>
         </ItemInfo>
       </ItemContents>
-
     </ItemContainer>
   );
 }
--- src/components/cartList/index.js
+++ src/components/cartList/index.js
@@ -5,10 +5,11 @@ import CartItem from '@components/cartList/CartItem';
 CartList.propTypes = {};
 
 function CartList({cartList}) {
-  if (!cartList) return;
   return (
     <Container>
-      {cartList.map((item, index) => <CartItem key={index} item={item}/>)}
+      {cartList.length === 0 ?
+        <p>장바구니가 비어있습니다.</p> :
+        cartList.map((item, index) => <CartItem key={index} item={item}/>)}
     </Container>
   );
 }
