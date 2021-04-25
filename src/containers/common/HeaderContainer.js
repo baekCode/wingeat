f5b61807f4df65bc756ca1dd160fdf2f5f822c4d
baekCode <baekcode@gmail.com>
2021-04-25 17:09:34 +0900

    feat : Header Container, CartCount

--- src/containers/common/HeaderContainer.js
+++ src/containers/common/HeaderContainer.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from '@components/Header';
+import {useSelector} from 'react-redux';
 
 HeaderContainer.propTypes = {};
 
 function HeaderContainer(props) {
-  return (
-    <>
-      <Header/>
-    </>
-  );
+  const [cartCount, setCartCount] = useState(0);
+  const {cart} = useSelector(({cart}) => ({cart: cart.cartList}));
+
+  useEffect(() => {
+    setCartCount(cart.length);
+  }, [cart]);
+
+  return <Header cartCount={cartCount}/>;
 }
 
 export default HeaderContainer;
