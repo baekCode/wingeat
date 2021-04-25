4df8b69bfa25a3c55c75e1160edcbec185377b46
baekCode <baekcode@gmail.com>
2021-04-25 19:30:21 +0900

    feat : config 환경변수

--- src/config/index.js
+++ src/config/index.js
@@ -8,6 +8,6 @@ dotenv.config();
  * REACT_APP_IMG_URL=https://image.wingeat.com/
  */
 export default {
-  API_URL: process.env.REACT_APP_API_URL,
-  IMG_URL: process.env.REACT_APP_IMG_URL
+  API_URL: process.env.REACT_APP_API_URL || 'https://node.wingeat.com/test-api/',
+  IMG_URL: process.env.REACT_APP_IMG_URL || 'https://image.wingeat.com/'
 };
