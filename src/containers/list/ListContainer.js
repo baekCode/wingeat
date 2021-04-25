1685d7a93f19a00e34b74f8195320ec02b0bf42d
baekCode <baekcode@gmail.com>
2021-04-25 17:09:15 +0900

    feat : List Container 인피니티 스크롤 구현 및 라우터 이동 이후 초기화 구현

--- src/containers/list/ListContainer.js
+++ src/containers/list/ListContainer.js
@@ -1,20 +1,22 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import List from '@components/list';
 import Title from '@components/title';
 import useIsMobile from '@hooks/useIsMobile';
-import {useDispatch, useSelector} from 'react-redux';
-import {getList} from '@modules/list';
+import {getList, initialList} from '@modules/list';
 
 ListContainer.propTypes = {};
 
-function ListContainer(props) {
+function ListContainer({history}) {
   const dispatch = useDispatch();
-  const {list, isInfinityPages} = useSelector(({list}) => ({
+  const {list, isInfinityPages, cart} = useSelector(({list, cart}) => ({
     list           : list.list,
-    isInfinityPages: list.isInfinityPages
+    isInfinityPages: list.isInfinityPages,
+    cart           : cart.cartList
   }));
-
   const [pageNumber, setPageNumber] = useState(1);
+  const [cartData, setCartData] = useState([]);
   const isMobile = useIsMobile(1023);
   const observer = useRef();
   const lastElementRef = useCallback(
@@ -31,8 +33,25 @@ function ListContainer(props) {
   );
 
   useEffect(() => {
+    const {location} = history;
+    return () => (location.pathname === '/') && dispatch(initialList());
+  }, [dispatch, history]);
+
+  useEffect(() => {
     dispatch(getList(pageNumber));
   }, [dispatch, pageNumber]);
+
+  useEffect(() => {
+    let storageData = JSON.parse(localStorage.getItem('cart'));
+    if (cart) {
+      if (!storageData) return setCartData([]);
+      return setCartData(cart.filter(item => storageData.includes(item.id)));
+    }
+  }, [cart]);
+
+  useEffect(() => {
+    console.log('cart', cart);
+  }, [cart]);
 
   return (
     <>
@@ -42,4 +61,4 @@ function ListContainer(props) {
   );
 }
 
-export default ListContainer;
+export default withRouter(ListContainer);
