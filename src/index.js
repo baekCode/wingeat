facc4dd03f6c5de626948434b058c02d848f6798
baekCode <baekcode@gmail.com>
2021-04-25 21:48:09 +0900

    feat : 첫 렌더링시 스토리지 로드

--- src/index.js
+++ src/index.js
@@ -8,11 +8,24 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer, {rootSaga} from '@modules/index';
 import App from '@/App';
 import GlobalStyle from '@lib/GlobalStyles';
+import {getCart} from '@modules/cart';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
+const loadStorage = storageKey => {
+  try {
+    const storageDate = localStorage.getItem(storageKey);
+    if (!storageDate) return;
+
+    store.dispatch(getCart(JSON.parse(storageDate)));
+  } catch (e) {
+    console.log('LocalStorage Error');
+  }
+};
+
 sagaMiddleware.run(rootSaga);
+loadStorage('cartList');
 
 ReactDOM.render(
   <Provider store={store}>
