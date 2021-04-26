e97daa38a922ed22f2b0b96918bfc500f7a6ca66
baekCode <baekcode@gmail.com>
2021-04-26 12:31:07 +0900

    chore : 주석제거

--- src/containers/cart/OrderContainer.js
+++ src/containers/cart/OrderContainer.js
@@ -5,8 +5,6 @@ import {useSelector} from 'react-redux';
 OrderContainer.propTypes = {};
 
 function OrderContainer(props) {
-  /*
-  * 리덕스 장바구니에서 선택된 값을 바라본다. 합계금액을 바라보면될듯 */
   const {payment} = useSelector(({payment}) => ({payment: payment.total}));
   return <Payment totalPrice={payment}/>;
 }
